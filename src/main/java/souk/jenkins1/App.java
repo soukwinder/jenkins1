6ad504b9c0ef1359e07d1355fbdd931d95b3eab9
soukwinder <sanjosinghmaddan@hotmail.com>
2016-02-01 12:09:13 +0100

    kk

--- src/main/java/souk/jenkins1/App.java
+++ src/main/java/souk/jenkins1/App.java
@@ -8,6 +8,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	//Wagenparkbeheer w1 = new Wagenparkbeheer();
+//    	CityGis c1 = new CityGis();
+//    	ThreadTest tt = new ThreadTest();
+//    	ThreadTest1 tt1 = new ThreadTest1();
+//    	ThreadTest2 tt2 = new ThreadTest2();
+//    	ThreadTest3 tt3 = new ThreadTest3();
+
+    	
+    	//zet de data in de database in 4 threads.
+//    	tt.HalfUnitId();
+//    	tt1.HalfUnitId();
+//    	tt2.HalfUnitId();
+//    	tt3.HalfUnitId();
+//    	tt.thread1.start();
+//    	tt1.thread2.start();
+//    	tt2.thread3.start();
+//    	tt3.thread4.start();
+    	
+    	
+//    	c1.getData();
+//    	c1.getTotalData();
+
+//		w1.getData();
+//		w1.saveData();
+		
+		//w1.test();
+
+//    	w1.ignition();
+//    	w1.com();
+//    	w1.com1();
     }
 }
